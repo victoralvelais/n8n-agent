d11b2bd6ae4516a7144f3c3231f8730d4e9da110
Victor Alvelais <6127592+victoralvelais@users.noreply.github.com>
2024-11-02 04:58:30 -0500

    add n8n config file

---
+++ n8n.config.ts
@@ -0,0 +1,22 @@
+export default {
+  "nodes": {
+    "communityNodes": [
+      // npm refs or github repos
+      "n8n-nodes-form-trigger",
+      "n8n-nodes-debughelper",
+      "n8n-nodes-document-generator",
+      "n8n-nodes-custom-credentials",
+    ]
+  },
+  // Example properties
+  // "path": {
+  //   "n8nFolder": ".n8n",
+  //   "customExtensions": "./custom-extensions"
+  // },
+  // "autoInstall": true,
+  // "n8n": {
+  //   "port": 5678,
+  //   "protocol": "http",
+  //   "host": "localhost"
+  // }
+}
